d29c6a87b0f0c9aefe9f21d326056bbf12cdea10
lightwick <smallyk99@gmail.com>
2023-02-01 19:00:38 +0900

    tiny fix-ups

--- MIMO_Rayleigh/simulate_mld.m
+++ MIMO_Rayleigh/simulate_mld.m
@@ -1,14 +1,14 @@
 function [BitErrorCount, SignalErrorCount] = simulate_mld(ReceivedSymbolSequence, SignalSequence, SignalBinary,  M, H)
     Nt = size(H,1);
+    NormalizationFactor = sqrt(2/3*(M-1)*Nt);
     persistent Candidates
     if isempty(Candidates)
-         Candidates = get_candidates(M, Nt);
+         Candidates = get_candidates(M, Nt) / NormalizationFactor;
     end
-    NormalizationFactor = sqrt(2/3*(M-1)*Nt);
-%     size(ReceivedSymbolSequence * ones(1,M^Nt))
-%     size(H*Candidates)
-    EuclideanDistance = abs(ReceivedSymbolSequence * ones(1,M^Nt) - H*Candidates).^2; % results in Nt x M^Nt, each column representing each candidate symbol combination
+    % results in Nt x M^Nt, each column representing each candidate symbol combination
+    EuclideanDistance = abs(ReceivedSymbolSequence * ones(1,M^Nt) - H*Candidates).^2;
     [val, idx] = min(sum(EuclideanDistance, 1));
+    
     DetectedBinary_MLD = reshape(de2bi(idx-1, log2(M)*Nt, 'left-msb'),log2(M),[])';
     DetectedSequence_MLD = bi2de(DetectedBinary_MLD, 'left-msb');
 
@@ -17,7 +17,6 @@ function [BitErrorCount, SignalErrorCount] = simulate_mld(ReceivedSymbolSequence
 end
 
 function Candidates = get_candidates(M, Nt)
-    NormalizationFactor = sqrt(2/3*(M-1)*Nt);
     AllNumbers = de2bi([0:M^Nt-1], Nt*log2(M), 'left-msb');
     Candidates = zeros(M^Nt, Nt);
     for ii = 1 : M^Nt
@@ -25,5 +24,5 @@ function Candidates = get_candidates(M, Nt)
             Candidates(ii,jj) = bi2de(AllNumbers(ii,log2(M)*(jj-1)+1:log2(M)*jj), 'left-msb');
         end
     end
-    Candidates = qammod(Candidates',M) / NormalizationFactor;
+    Candidates = qammod(Candidates',M);
 end
