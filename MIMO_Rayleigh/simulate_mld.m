22e8fb7ce968b4f81a6aff1b0a3473060024e89c
lightwick <smallyk99@gmail.com>
2023-02-01 19:15:52 +0900

    tiny fixes on order

--- MIMO_Rayleigh/simulate_mld.m
+++ MIMO_Rayleigh/simulate_mld.m
@@ -1,12 +1,14 @@
 function [BitErrorCount, SignalErrorCount] = simulate_mld(ReceivedSymbolSequence, SignalSequence, SignalBinary,  M, H, EsN0)
     Nt = size(H,1);
+    NormalizationFactor = sqrt(2/3*(M-1)*Nt);
     persistent Candidates
     if isempty(Candidates)
-         Candidates = get_candidates(M, Nt);
+         Candidates = get_candidates(M, Nt) / NormalizationFactor;
     end
-    NormalizationFactor = sqrt(2/3*(M-1)*Nt);
-    EuclideanDistance = abs(ReceivedSymbolSequence/sqrt(EsN0) * ones(1,M^Nt) - H*Candidates/NormalizationFactor).^2; % results in Nt x M^Nt, each column representing each candidate symbol combination
+    
+    EuclideanDistance = abs(ReceivedSymbolSequence/sqrt(EsN0) * ones(1,M^Nt) - H*Candidates).^2; % results in Nt x M^Nt, each column representing each candidate symbol combination
     [val, idx] = min(sum(EuclideanDistance, 1));
+    
     DetectedBinary_MLD = reshape(de2bi(idx-1, log2(M)*Nt, 'left-msb'),log2(M),[])';
     DetectedSequence_MLD = bi2de(DetectedBinary_MLD, 'left-msb');
 
--- MIMO_Rayleigh/simulate_mmse.m
+++ MIMO_Rayleigh/simulate_mmse.m
@@ -2,10 +2,10 @@ function [BitErrorCount, SignalErrorCount] = simulate_mmse(ReceivedSymbolSequenc
     Nt = size(H,1);
     NormalizationFactor = sqrt(2/3*(M-1) * Nt); % size(H,1) = Nt
 
-    w_mmse = NormalizationFactor * inv(H' * H + Nt / EsN0 * eye(Nt)) * H';
+    w_mmse = NormalizationFactor / sqrt(EsN0) * inv(H' * H + Nt / EsN0 * eye(Nt)) * H';
     DetectedSymbolSequence_MMSE = w_mmse * ReceivedSymbolSequence; % Detection (Zero-Forcing: y / h)
 
-    DetectedSignalSequence_MMSE = qamdemod(DetectedSymbolSequence_MMSE/sqrt(EsN0), M); % Detection
+    DetectedSignalSequence_MMSE = qamdemod(DetectedSymbolSequence_MMSE, M); % Detection
     DetectedBinary_MMSE = de2bi(DetectedSignalSequence_MMSE, log2(M), 'left-msb');
 
     BitErrorCount = sum(SignalBinary~=DetectedBinary_MMSE, 'all');
