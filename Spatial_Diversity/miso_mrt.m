431647d0abaa993fbc0ffab0a224047f493f7aa1
lightwick <smallyk99@gmail.com>
2023-02-17 16:22:23 +0900

    Finished MRT prototype(Unnormalized)

---
+++ Spatial_Diversity/miso_mrt.m
@@ -0,0 +1,21 @@
+function [BitErrorCount, SignalErrorCount] = miso_mrt(SignalSequence, Noise, SignalBinary,  M, H)
+    % TODO: Normalization
+    Nt = size(H,2);
+    Nr = size(H,1);
+    assert(Nt==2 && Nr==1, "Need H of size 1x2")
+
+    NormalizationFactor = sqrt(2/3*(M-1) * Nt * 2);
+
+    w = H';
+    TransmitSymbol = w*SignalSequence;
+    ReceivedSymbol = H*TransmitSymbol + Noise;
+    
+%     y = ReceivedSymbol;
+    
+    FrobSquared = H*H'; % equivalent to norm(H,'fro')^2
+    DetectedSignal = qamdemod(ReceivedSymbol/FrobSquared, M);
+    DetectedBinary = de2bi(DetectedSignal, log2(M), 'left-msb');
+    
+    SignalErrorCount = sum(DetectedSignal~=SignalSequence, 'all');
+    BitErrorCount = sum(SignalBinary~=DetectedBinary, 'all');
+end
