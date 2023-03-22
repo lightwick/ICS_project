7a4f0d22f89c9c01ce26d73bc47a70ef3892593a
lightwick <smallyk99@gmail.com>
2023-03-23 08:53:28 +0900

    consistency among tools

--- tools/simulate_mld.m
+++ tools/simulate_mld.m
@@ -1,26 +1,59 @@
-function [BitErrorCount, SignalErrorCount] = simulate_mld(ReceivedSymbolSequence, SignalSequence, SignalBinary,  M, H)
-    Nt = size(H,2);
+function [BER, SER] = get_mld_error_rate(M, iteration, EbN0_dB)
+    EbN0 = db2pow(EbN0_dB);
+    EsN0 = EbN0 * log2(M);
+    EsN0_dB = pow2db(EsN0);
+    
+    %% DEBUG
+    Nt = 2;
+    Nr = 2;
     NormalizationFactor = sqrt(2/3*(M-1)*Nt);
+    
+    %% Timer
+    FivePercent = ceil(iteration/20);
+    
+    %% START
     persistent Candidates
     if isempty(Candidates)
-%          Candidates = get_candidates(M, Nt) / NormalizationFactor;
-         Candidates = zeros(M^Nt, Nt);
-         for ii = 0:M^Nt-1
-             for jj = 1:Nt
-                 Candidates(ii+1,jj) = mod(floor(ii/M^(Nt-jj)),M);
-             end
-         end
-         Candidates = qammod(Candidates', M) / NormalizationFactor;
+         Candidates = get_candidates(M, Nt) / NormalizationFactor;
     end
-    % results in Nt x M^Nt, each column representing each candidate symbol combination
-    EuclideanDistance = abs(ReceivedSymbolSequence * ones(1,M^Nt) - H*Candidates).^2;
-    [~, idx] = min(sum(EuclideanDistance, 1));
     
-    DetectedBinary_MLD = reshape(de2bi(idx-1, log2(M)*Nt, 'left-msb'),log2(M),[])';
-    DetectedSequence_MLD = bi2de(DetectedBinary_MLD, 'left-msb');
+    BEC = zeros(length(EsN0), 1);
+    SEC = zeros(length(EsN0), 1);
+    
+    
+    for iTotal = 1:iteration
+        if mod(iTotal-100, FivePercent)==0
+            tic
+        end
+        % Bit Generation
+        SignalSequence = randi([0 M-1], Nt, 1);
+        SignalBinary = de2bi(SignalSequence, log2(M), 'left-msb');
+        SymbolSequence = qammod(SignalSequence, M) / NormalizationFactor;
+        H = (randn(Nr, Nt) + 1j * randn(Nr, Nt)) ./ sqrt(2); % Receiver x Transmitter
+        NoiseSequence = (randn(Nr, 1) + 1j * randn(Nr, 1)) / sqrt(2); % Noise (n) Generation
+        
+        for EsN0_idx = 1:length(EsN0)
+            ReceivedSymbol = H * SymbolSequence + NoiseSequence * sqrt(1 / EsN0(EsN0_idx)); % log2(M)x1 matrix
+            % results in Nt x M^Nt, each column representing each candidate symbol combination
+            EuclideanDistance = abs(ReceivedSymbol * ones(1,M^Nt) - H*Candidates).^2;
+            [~, idx] = min(sum(EuclideanDistance, 1));
 
-    BitErrorCount = sum(SignalBinary~=DetectedBinary_MLD, 'all');
-    SignalErrorCount = sum(SignalSequence~=DetectedSequence_MLD, 'all');
+            DetectedBinary = reshape(de2bi(idx-1, log2(M)*Nt, 'left-msb'),log2(M),[])';
+            DetectedSequence = bi2de(DetectedBinary, 'left-msb');
+
+            BEC(EsN0_idx) = BEC(EsN0_idx) + sum(SignalBinary~=DetectedBinary, 'all');
+            SEC(EsN0_idx) = SEC(EsN0_idx) + sum(SignalSequence~=DetectedSequence, 'all');
+        end
+        
+        if mod(iTotal-100, FivePercent)==0
+            ElapsedTime = toc;
+            EstimatedTime = (iteration-iTotal)*ElapsedTime;
+            disp(sprintf("%d%%, estimated wait time %d minutes %d seconds", round(iTotal/iteration*100), floor(EstimatedTime/60), floor(mod(EstimatedTime, 60))))
+        end
+    end
+    
+    SER = SEC/(iteration*2);
+    BER = BEC/(iteration*2*log2(M));
 end
 
 function Candidates = get_candidates(M, Nt)
@@ -31,5 +64,5 @@ function Candidates = get_candidates(M, Nt)
             Candidates(ii,jj) = bi2de(AllNumbers(ii,log2(M)*(jj-1)+1:log2(M)*jj), 'left-msb');
         end
     end
-    Candidates = qammod(Candidates', M);
+    Candidates = qammod(Candidates',M);
 end
