0b7a5d69e5f98514f183ccef1b88c9346588d9b2
lightwick <smallyk99@gmail.com>
2023-07-09 17:42:13 +0900

    finished first draft(debuggin required)

--- STBC_SM/codebook_gen.m
+++ STBC_SM/codebook_gen.m
@@ -8,7 +8,7 @@
 
 % USE EXAMPLE: mapping(3){1}{2}; Nt=3, first codebook, second pair
 function mapping = codebook_gen()
-    key=[3,4,8]
+    key=[3,4,8,2]
     % Value for 3
     value{1} = {
 		    {[1,2]},
@@ -26,6 +26,9 @@ function mapping = codebook_gen()
 		    {[1,5],[2,6],[3,7],[4,8]},
             {[1,3],[2,4],[5,7],[6,8]}
 	    };
-    
+    % Value for Nt=2; Existing only for debugging purposes
+    value{4} = {
+		    {[1,2]}
+	    };
     mapping=containers.Map(key,value);
 end
--- STBC_SM/main.m
+++ STBC_SM/main.m
@@ -1,20 +1,38 @@
+clear all
+clc
+
 %% TODO
 % check if input configuration is viable with the code
 % condition 1: in M=16, n_t<=8
 
-%% Basic Configuration
+%% Current STATUS
+% When Nt=2, no BER==0; meaning there is a problem when SM is added
+
+%% DEBUGGING FEATURES
+DEBUG = true;
+NOISE = false;
+
+%% User Defined Configuration
 addpath('../tools');
 M = 16;
-EsN0_dB = 0:10;
+EsN0_dB =  0:2:18;
+if DEBUG==true
+    EsN0_dB=0;
+end
 
-EsN0_config % configuration script in tools
-
-Nt = 8;
+Nt = 4;
 Nr = Nt;
 
-iteration = 1;
+iteration = 10^4;
 
-NormalizationFactor = sqrt(2/3*(M-1) * Nt);
+
+%% Basic Configuration
+EsN0_config % configuration script in tools
+
+num = 1;
+BEC = zeros(num, length(EsN0_dB));
+
+NormalizationFactor = sqrt(2/3*(M-1) * 2); % Normally, this would be 'NormalizationFactor = sqrt(2/3*(M-1) * Nt);', but Spatial Modulation is added here
 
 %% STBC-SM
 map = codebook_gen();
@@ -55,6 +73,17 @@ else
     end
 end
 
+%% Modulated Candidate Generation
+% Candidates = zeros(M^2, 2);
+% 
+%  for ii = 0:M^2-1
+%      for jj = 1:2
+%          Candidates(ii+1,jj) = mod(floor(ii/M^(2-jj)),M);
+%      end
+%  end
+%  Candidates = qammod(Candidates', M) / NormalizationFactor; % Each column is a Candidate
+Candidates = qammod(0:M-1, M) / NormalizationFactor;
+
 %% Main simulation process
 % Timer
 FivePercent = ceil(iteration/20);
@@ -66,6 +95,11 @@ for iTotal = 1:iteration
     %% Bit Generation
     % Transmitter Defined Bits
     TransmitterDecimal = randi([0 c-1]);
+    TransmitterDecimal = 2;
+    if Nt~=2
+        TransmitterBinary = de2bi(TransmitterDecimal, log2(c), 'left-msb');
+    end
+
     CodebookIndex = floor(TransmitterDecimal/a)+1;
     CodewordIndex = mod(TransmitterDecimal, a)+1;
 
@@ -75,17 +109,103 @@ for iTotal = 1:iteration
     
     
     %% Coding
-    STBC = [SymbolSequence'; -conj(SymbolSequence(2,1)) conj(SymbolSequence(1,1))].';
+    STBC = [SymbolSequence.'; -conj(SymbolSequence(2,1)) conj(SymbolSequence(1,1))];
     STBC_SM = zeros(2,Nt);
     
-    STBC_SM(:,codebook{CodebookIndex}{CodewordIndex}) = STBC * theta(CodebookIndex);
+    STBC_SM(:, codebook{CodebookIndex}{CodewordIndex}) = STBC * exp(1j*theta(CodebookIndex));
     
     H = (randn(Nt, Nr) + 1j * randn(Nt, Nr)) ./ sqrt(2); % Receiver x Transmitter
-    Noise = (randn(2, Nr) + 1j * randn(2, Nr)) / sqrt(2);
-    ReceivedSignal = STBC_SM * H + Noise;
-    %% Decoding
-    
+    if NOISE==false
+        Noise = zeros(2,Nr);
+    else
+        Noise = (randn(2, Nr) + 1j * randn(2, Nr)) / sqrt(2);
+    end
 
+    H_l = zeros(Nr*2, 2, c);
+
+    %% Create H_l
+    for book=1:n
+        assert(a==last_a, 'The current implementation needs a==last_a. Check configuration or improve code')
+        for word=1:a % This should be fixed if a~=last_a
+            H_t = H.'; % transpose so Nr x Nt
+            tmp_1 = H_t(:, codebook{book}{word}) * exp(1j*theta(book));
+            tmp_2 = conj(tmp_1(:, [2, 1]));
+            tmp_2(:, 2) = -tmp_2(:, 2);
+
+            H_l(1:2:Nr*2, :, word+(book-1)*a) = tmp_1;
+            H_l(2:2:Nr*2, :, word+(book-1)*a) = tmp_2;
+        end
+    end
+
+
+    for SNR_idx = 1 : length(EsN0)
+        ReceivedSignal = STBC_SM * H + Noise / sqrt(EsN0(SNR_idx)); % shape of 2xNr
+        y = reshape(ReceivedSignal, [], 1);
+        if DEBUG==true
+            disp('before')
+            y
+        end
+
+        y(2:2:size(y,1), 1) = conj(y(2:2:size(y,1), 1));
+        if DEBUG==true
+            disp('after')
+            y
+        end
+
+        x1 = zeros(M, c);
+        x2 = zeros(M, c);
+        for c_idx = 1:c
+            for m=1:M
+                x1(m, c_idx) = norm(y-H_l(:, 1, c_idx)*Candidates(m), 'fro')^2;
+                x2(m, c_idx) = norm(y-H_l(:, 2, c_idx)*Candidates(m), 'fro')^2;
+            end
+        end
+        [minVal, minIndex]= min(x1, [], 'all');
+        [detected_x1, ~] = ind2sub(size(x1), minIndex);
+
+        [minVal, minIndex]= min(x2, [], 'all');
+        [detected_x2, ~] = ind2sub(size(x2), minIndex);
+
+        m_l = min(x1)+min(x2);
+        [~, detected_l] = min(m_l);
+
+        % Due to the starting index being 1, 1 is subtracted
+        detected_x1 = detected_x1-1;
+        detected_x2 = detected_x2-1;
+
+        DetectedSequence = [detected_x1; detected_x2];
+        DetectedBinary = de2bi(DetectedSequence, log2(M), 'left-msb');
+
+        detected_l = detected_l-1; % Due to the starting index being 1, 1 is subtracted
+        if Nt~=2
+            DetectedTransmitterBinary = de2bi(detected_l, log2(c), 'left-msb');
+        end
+        
+        if Nt~=2
+            ErrorCount = sum((SignalBinary~=DetectedBinary), 'all') + sum((TransmitterBinary~=DetectedTransmitterBinary), 'all');
+        else
+            ErrorCount = sum((SignalBinary~=DetectedBinary), 'all');
+        end
+
+        % if DEBUG==true
+        %     c_tmp = CodewordIndex+(CodebookIndex-1)*a;
+        %     assert(isequal(H_l(:, 1, c_tmp)*Candidates(SignalSequence(1,1)+1) + H_l(:, 2, c_tmp)*Candidates(SignalSequence(2,1)+1),y), 'non match');
+        % end
+        found = false;
+        for ii_test=1:c
+            if isequal(H_l(:, 1, ii_test)*Candidates(SignalSequence(1,1)+1) + H_l(:, 2, ii_test)*Candidates(SignalSequence(2,1)+1),y)
+                found = true;
+            end
+        end
+        assert(found==true, 'not found');
+        
+        BEC(1, SNR_idx) = BEC(1, SNR_idx) + ErrorCount; %bitcount(bitxor([detected_x1; detected_x2], SignalSequence)));
+        
+        % if ErrorCount~=0
+        %     disp(sum((SignalBinary~=DetectedBinary), 'all'))
+        %     disp(sum((TransmitterBinary~=DetectedTransmitterBinary), 'all'))
+        % end
+    end
 
     if mod(iTotal-100, FivePercent)==0
         ElapsedTime = toc;
@@ -93,3 +213,13 @@ for iTotal = 1:iteration
         disp(sprintf("%d%%, estimated wait time %d minutes %d seconds", round(iTotal/iteration*100), floor(EstimatedTime/60), floor(mod(EstimatedTime, 60))))
     end
 end
+TotalTransmitBits = (2 * log2(M) + log2(c))* iteration;
+BER(1,:) = BEC(1,:)/TotalTransmitBits;
+
+%% Plotting
+BER_Title = sprintf("BER for %d-QAM", M);
+x_axis = "SNR (dB)";
+
+legend_order = ["STBC-SM, n_T=8, 16-QAM, 6 bits/s/Hz"];
+myplot(EsN0_dB, BER, BER_Title, x_axis, "BER", legend_order);
+ylim([10^(-6) 1])
