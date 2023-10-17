95d0e29e1c5c096bc436681a943b925b5d647ba2
lightwick <smallyk99@gmail.com>
2023-10-17 19:08:32 +0900

    added different markers

--- tools/myplot.m
+++ tools/myplot.m
@@ -1,11 +1,13 @@
 function fig = myplot(x, y, title_in, xlabel_in, ylabel_in, legend_in)
     colors = ["#0000FF", "#FF0000", "#4DBEEE", "#D95319", "#77AC30", "#EDB120", "#7E2F8E"];
-    
+    Marker = [".", "pentagram", "hexagram", "*", "square", "^", "diamond"]
+    MarkerSize = 8 * ones(1, length(colors));
+    MarkerSize(1) = 15;
+
     fig = figure();
-    semilogy(x, y(1,:), '.--', 'Color', colors(1),'MarkerSize', 15);
-    hold on
-    for ii=2:size(y,1)
-        semilogy(x, y(ii,:), '.--','Color', colors(ii), 'MarkerSize', 15);
+    for ii=1:size(y,1)
+        semilogy(x, y(ii,:), '--','Color', colors(ii), 'Marker', Marker(ii), 'MarkerSize', MarkerSize(ii));
+        hold on
     end
     ylabel(ylabel_in);
     title(title_in);
