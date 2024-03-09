6bf081bfbb64c6da1b012f3267961810500addb5
Kevin Noh <smallyk99@gmail.com>
2024-03-09 23:02:10 +0900

    24/03/09 Generalized code and removed redundancy

--- ModulationDiversity_SM/Decoding_Complexity/MA_SM.m
+++ ModulationDiversity_SM/Decoding_Complexity/MA_SM.m
@@ -1,8 +1,14 @@
+%% Description
+% Same system model as MA-SM
+% Using Nt=4, Np=2, utilizing two time frames to get more accurate
+% estimation of antenna detection
+
 close all
 clear all
 clc
 
 %% TODO
+% I could possibly generalize the code for flexible time slot use
 
 %% Current STATUS
 % When Nt=2, no BER==0; meaning there is a problem when SM is added
@@ -12,16 +18,18 @@ addpath('../../tools');
 
 % NONOISE = input("No noise?");
 
-EsN0_dB =  0:2:20;
+EsN0_dB =  0:2:24;
 % EsN0_dB = 0;
 EsN0 = db2pow(EsN0_dB);
+
+TimeSlot = 4;
 
 Nt = 4;
 Np = 2;
 Nr = 4;
 M = 4;
 
-iteration = 10^5;
+iteration = 10^6;
 
 num = 1;
 BEC = zeros(num, length(EsN0_dB));
@@ -62,16 +70,16 @@ for iTotal = 1:iteration
 
     H = (randn(Nr, Nt) + 1j * randn(Nr, Nt)) / sqrt(2);
     n = (randn(Nr, 1) + 1j * randn(Nr, 1)) / sqrt(2);
-    SignalDecimal = randi([0 M-1], Np, 1);
+    SignalDecimal = randi([0 M-1], Np, TimeSlot);
 
     theta = exp(1j*[0, pi/8, pi/4, 3*pi/8]);
     x = qammod(SignalDecimal, M, 'UnitAveragePower', true) / sqrt(Np) * theta(TransmittedCodeword_idx);
-    
-    x_eff = zeros(Nt, 1);
-    x_eff(AntSelection) = x;
+
+    x_eff = zeros(Nt, TimeSlot);
+    x_eff(AntSelection, :) = x;
     %% SM Application
 
-    % n = zeros(size(n));
+%     n = zeros(size(n));
 
     for SNR_idx = 1 : length(EsN0)
         y = H * x_eff + n / sqrt(EsN0(SNR_idx));
@@ -81,37 +89,49 @@ for iTotal = 1:iteration
         for i1=1:length(codebook)
             T_k(:,:,i1) = H(:, codebook(i1, :));
         end
+%         T_k_hermitian = pagectranspose(T_k);
+%         Inverse = pageinv(pagemtimes(T_k_hermitian, T_k));
+%         
+%         First = pagemtimes(T_k, Inverse);
+%         Second = pagemtimes(First, T_k_hermitian);
+%         Final = pagemtimes(Second, y);
+%         
+%         EndNorm = pagenorm(Final, 'fro');
+
+        % [val, idx] = sort(EndNorm(:), 'descend');
+        % idx = sort(idx(1:Np));
+
+        %% First Time Slot
         T_k_hermitian = pagectranspose(T_k);
         Inverse = pageinv(pagemtimes(T_k_hermitian, T_k));
         
         First = pagemtimes(T_k, Inverse);
         Second = pagemtimes(First, T_k_hermitian);
-        Final = pagemtimes(Second, y);
-        
-        EndNorm = pagenorm(Final, 'fro');
 
-        % [val, idx] = sort(EndNorm(:), 'descend');
-        % idx = sort(idx(1:Np));
-        [val, idx] = max(EndNorm(:));
+        EndNormSum = 0;
+        for i1=1:TimeSlot
+            FirstFinal = pagemtimes(Second, y(:, i1));
+            EndNorm = pagenorm(FirstFinal, 'fro');
+            EndNormSum = EndNormSum + EndNorm;
+        end
+        [val, idx] = max(EndNormSum);
+
+        %% Test
         DetectedWord_idx = idx;
 
-        % for i1 = 1:c
-        %     if codebook(i1,:)==idx.'
-        %         DetectedWord_idx = i1;
-        %         break
-        %     end
-        % end
-
-        Candidate_y = zeros(Nt, length(ModulatedCandidates));
+        Candidate_x = zeros(Nt, length(ModulatedCandidates));
         for i1=1:length(ModulatedCandidates)
-            Candidate_y(codebook(idx,:), i1) = ModulatedCandidates(i1,:);
+            Candidate_x(codebook(idx,:), i1) = ModulatedCandidates(i1,:);
         end
 
-        Candidate_y = H * Candidate_y * theta(DetectedWord_idx);
-        diff = sum(abs(y-Candidate_y).^2, 1);
-        [val, idx] = min(diff);
-
-        DetectedBinary = de2bi(Candidates(idx, :), log2(M));
+        Candidate_y = H * Candidate_x * theta(DetectedWord_idx);
+        DetectedSignal = [];
+        for i1=1:TimeSlot
+            diff = sum(abs(y(:, i1)-Candidate_y).^2, 1);
+            [~, idx] = min(diff);
+            DetectedSignal = [DetectedSignal Candidates(idx, :)];
+        end
+        DetectedBinary = de2bi(DetectedSignal, log2(M));
         SignalBinary = de2bi(SignalDecimal, log2(M));
         SignalError = sum((SignalBinary~=DetectedBinary), 'all');
         
@@ -130,17 +150,18 @@ for iTotal = 1:iteration
     end
 end
 
-TotalTransmitBits = (Np * log2(M) + log2(c))* iteration;
+TotalTransmitBits = (Np * log2(M) * TimeSlot + log2(c)) * iteration;
 
 BER(1,:) = BEC(1,:)/TotalTransmitBits;
 TBER(1,:) = TBEC(1,:)/(log2(c)*iteration);
 SBER(1,:) = SBEC(1,:)/(2*log2(M)*iteration);
 
 %% Plotting
-BER_Title = "STBC-SM";
+BER_Title = "MA-SM";
 x_axis = "SNR (dB)";
 
 proposed_legend = sprintf("STBC-SM, n_T=%d, %d-QAM", Nt, M);
-legend_order = ["STBC-SM 64-QAM(7 bpcu)", "MD-SM Orthogonal(9 bpcu)", "STBC-SM 16-QAM(5 bpcu)"];
+% legend_order = ["STBC-SM 64-QAM(7 bpcu)", "MD-SM Orthogonal(9 bpcu)", "STBC-SM 16-QAM(5 bpcu)"];
 myplot(EsN0_dB, BER, BER_Title, x_axis, "BER", legend_order);
-ylim([10^(-6) 1])
+ylim([10^(-5) 1])
+xlim([EsN0_dB(1) EsN0_dB(length(EsN0_dB))]);
